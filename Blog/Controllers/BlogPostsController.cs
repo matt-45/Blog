141006bf84771d733f0ae13c8fb8f5988893aebc
matt-45 <53614616+matt-45@users.noreply.github.com>
2019-10-15 16:49:10 -0400

    Got image uploading working

---
+++ Blog/Controllers/BlogPostsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Helpers;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BlogPosts
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.BlogPosts.ToList());
+        }
+
+        public ActionResult Archive()
+        {
+            var blogPosts = db.BlogPosts.Where(b => b.Published).ToList();
+
+            return View(blogPosts);
+        }
+
+        // GET: BlogPosts/Details/5
+        public ActionResult Details(string Slug)
+        {
+            if (String.IsNullOrWhiteSpace(Slug))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.FirstOrDefault(p => p.Slug == Slug);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+        [Authorize(Roles = "Admin")]
+        // GET: BlogPosts/Create
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogPosts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Title,Abstract,BlogPostBody,Published")] BlogPost blogPost, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                var Slug = StringUtilites.URLFriendly(blogPost.Title);
+                if (String.IsNullOrWhiteSpace(Slug))
+                {
+                    ModelState.AddModelError("Title", "Invalid title");
+                    return View(blogPost);
+                }
+                if (db.BlogPosts.Any(p => p.Slug == Slug))
+                {
+                    ModelState.AddModelError("Title", "The title must be unique");
+                    return View(blogPost);
+                }
+
+                if (ImageUploadValidator.IsWebFriendlyImage(image))
+                {
+                    var fileName = $"{DateTime.Now.Ticks}_{Path.GetFileName(image.FileName)}";
+                    image.SaveAs(Path.Combine(Server.MapPath("~/Uploads/"), fileName));
+                    blogPost.MediaURL = "/Uploads/" + fileName;
+                }
+
+                blogPost.Slug = Slug;
+                blogPost.Created = DateTime.Now;
+                db.BlogPosts.Add(blogPost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(string slug)
+        {
+            if (slug == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.FirstOrDefault(b => b.Slug == slug);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Abstract,BlogPostBody,Published")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var post = db.BlogPosts.First(a => a.Id == blogPost.Id);
+                var Slug = StringUtilites.URLFriendly(blogPost.Title);
+                if (String.IsNullOrWhiteSpace(Slug))
+                {
+                    ModelState.AddModelError("Title", "Invalid title");
+                    return View(blogPost);
+                }
+
+                post.Slug = Slug;
+                post.Title = blogPost.Title;
+                post.Abstract = blogPost.Abstract;
+                post.Updated = DateTime.Now;
+                post.BlogPostBody = blogPost.BlogPostBody;
+                post.Comments = blogPost.Comments;
+
+                db.SaveChanges();
+                return RedirectToAction("");
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            db.BlogPosts.Remove(blogPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Blog.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+
+// These are different .NET frameworks.
+
+namespace Blog.Controllers // all controllers are inside this namespace
+{
+    public class HomeController : Controller { 
+/*var data = new dataVM()
+            {
+                data.
+            }*/
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+
+            var blogPosts = db.BlogPosts.Where(b => b.Published).ToList();
+
+            return View(blogPosts);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            EmailModel model = new EmailModel();
+            return View(model);
+        }
+
+    }
+}
